9dd175d43cb421df3a09a678d98f6a49e959cdea
vagrant <vagrant@homestead>
2018-09-03 16:34:17 +0000

    Finish basic page layout

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            由 <a href="https://weibo.com/u/2256969747" target="_blank">lizhen9880</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+
+        <p class="pull-right"><a href="mailto:lizhen9880@126.com">联系我们</a></p>
+    </div>
+</footer>
