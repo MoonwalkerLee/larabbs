49f33e3b4fcc98096ee11c1c434735bc75f36c86
vagrant <vagrant@homestead>
2018-10-25 09:25:30 +0000

    Admin can manage site settings

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -4,6 +4,6 @@
             由 <a href="https://weibo.com/u/2256969747" target="_blank">lizhen9880</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
         </p>
 
-        <p class="pull-right"><a href="mailto:lizhen9880@126.com">联系我们</a></p>
+        <p class="pull-right"><a href="mailto:{{ setting('contact_email') }}">联系我们</a></p>
     </div>
 </footer>
